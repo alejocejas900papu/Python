2376ab148de332478ab5faa0f7869eccd5e966ab
alejocejas900papu <127849583+alejocejas900papu@users.noreply.github.com>
2023-03-14 11:18:19 -0300

    trabajo practico numero dos

---
+++ Tp2.py
@@ -0,0 +1,12 @@
+#Descripcion de Trabajo
+print("-------------------------------")
+print("Ejercicio 2 promedio de un alumno")
+print("-------------------------------")
+#Ingreso de datos
+nota1=int (input("Ingrese la primera nota "))
+nota2=int (input("Ingrese la segundo nota "))
+nota3=int (input("Ingrese la tercero nota "))
+#Proceso
+promedio=(nota1+nota2+nota3)/3
+#Solida
+print ("El promedio del alumno es ", promedio)
