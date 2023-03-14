f6d91fc1d0854ebc19ab99c5111f4d78f2f4488f
alejocejas900papu <127849583+alejocejas900papu@users.noreply.github.com>
2023-03-14 11:14:37 -0300

    ejercicio numero 1

---
+++ Tp1.py
@@ -0,0 +1,12 @@
+#Descripcion de Trabajo
+print("-------------------------------")
+print("Ejercicio 1 CALCULAR Distacia")
+print("-------------------------------")
+#Ingreso de datos
+print("Ingrese la velocidad y tiempo de la unidad movil")
+V= float (input("Velocidad en Km:"))
+T= int (input("Tiempo en Horas:"))
+#Proceso
+D=V*T
+#Solida
+print ("Se recorreran ",D, " Km por Hora")
