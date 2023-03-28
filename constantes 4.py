05afe0e22171357e85f923e5c8dcf91b0a13aee0
alejocejas900papu <127849583+alejocejas900papu@users.noreply.github.com>
2023-03-28 11:17:29 -0300

    constantes 4

---
+++ constantes 4.py
@@ -0,0 +1,11 @@
+import math
+from re import T
+print ("-------------------------------")
+print ("Ejercicio Condicioneal nuemero 4 ")
+print ("-------------------------------")
+
+t=int (print("ingrese el tiempo en segundos"))
+d=int (print("ingrese ña distancia em metros "))
+
+v=d/t
+print (" la velocidad :" ,v, "m/s")
