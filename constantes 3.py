06bb134e0620bbf8187498bcb29f555eb048dcc3
alejocejas900papu <127849583+alejocejas900papu@users.noreply.github.com>
2023-03-28 11:17:08 -0300

    constantes 3

---
+++ constantes 3.py
@@ -0,0 +1,16 @@
+import math
+print ("-------------------------------")
+print ("Ejercicio Condicioneal nuemero 3 ")
+print ("-------------------------------")
+
+do=2.83
+eu=3.84
+
+soles=int (print("ingresa la cantidad de soles"))
+
+d= soles / do
+e= soles / eu 
+
+print (" en ", soles , " soles hay ", e , "euros ")
+print ("en ", soles , " soles hay ", d , " dolares")
+
