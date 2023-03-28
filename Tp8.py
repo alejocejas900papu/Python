d2713d7969a52cdab0af3a3b08a94454302260a2
alejocejas900papu <127849583+alejocejas900papu@users.noreply.github.com>
2023-03-28 09:32:51 -0300

    trabajo practico 7

---
+++ Tp8.py
@@ -0,0 +1,14 @@
+#Descripcion de Trabajo
+import math
+print("-------------------------------")
+print("Ejercicio 8")
+print("-------------------------------")
+#Ingreso de datos
+BASE=int (input("Ingrese la base del rectangulo "))
+ALTO=int (input("Ingrese la altura del rectangulo "))
+#Proceso
+SUP=BASE*ALTO
+PER=2*(BASE+ALTO)
+#Solida
+print ("El Perimetro es ", PER)
+print ("La Superficie es ", SUP)
