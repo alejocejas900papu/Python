f1734ea288ee8363d2de068ec819c69b0322d419
alejocejas900papu <127849583+alejocejas900papu@users.noreply.github.com>
2023-03-28 11:16:45 -0300

    constantes 2

---
+++ constantes  2.py
@@ -0,0 +1,10 @@
+import math
+print ("-------------------------------")
+print ("Ejercicio Condicioneal nuemero 2 ")
+print ("-------------------------------")
+
+print ("Ingrese altura del triángulo:")
+
+h=int (print("ingrese la altura"))
+p =3*(2*h/3^0.5)
+print ("El perímetro del triangulo será:",p)
