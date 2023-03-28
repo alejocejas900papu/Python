4fc3d0c29b3b2f5edc521131c222547fc2350ccb
alejocejas900papu <127849583+alejocejas900papu@users.noreply.github.com>
2023-03-28 09:33:55 -0300

    trabajo practico 9

---
+++ Tp9.py
@@ -0,0 +1,14 @@
+#Descripcion de Trabajo
+import math
+print("-------------------------------")
+print("Ejercicio 9")
+print("-------------------------------")
+#Constante
+LITXG=3.785
+PRECIOXL=4.50
+#Ingreso de datos
+CONSU=float (input("Ingrese consumo: "))
+#Proceso
+TOTAL= CONSU*LITXG*PRECIOXL
+#Solida
+print ("El Total es ", TOTAL)
