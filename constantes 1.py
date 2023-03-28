28431b773da5572a83dab90715f6754416335f9e
alejocejas900papu <127849583+alejocejas900papu@users.noreply.github.com>
2023-03-28 11:16:24 -0300

    constantes 1

---
+++ constantes 1.py
@@ -0,0 +1,12 @@
+import math 
+print ("-------------------------------")
+print ("Ejercicio Condicioneal nuemero 1 ")
+print ("-------------------------------")
+
+Pi=3.1416
+
+radio=input(print("ingrese el radio que queres que tenga la esfera"))
+
+v=4/3*Pi*radio^3
+
+print("el volumen de la esfera es de ", v)
