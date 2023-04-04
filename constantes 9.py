2fd45053e1ffbf7a2b50a18f2cfe918d0661264c
alejocejas900papu <127849583+alejocejas900papu@users.noreply.github.com>
2023-04-04 10:49:21 -0300

    constantes 9

---
+++ constantes 9.py
@@ -0,0 +1,19 @@
+import math
+from re import T
+print ("-------------------------------")
+print ("Ejercicio Condicioneal nuemero 9 ")
+print ("-------------------------------")
+
+x=float 
+
+pi=3.1416
+
+print ("ingrese angulo en radiantes :") 
+ x=float (input())
+
+ sex = x*180/ pi 
+ cen = x*220/pi 
+
+print (" en sexagesimales es : ", sex)
+print (" rn centesimales es : " , cen )
+
