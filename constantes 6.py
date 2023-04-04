624368133289fe5b60c2838a1a2ed077eee30a34
alejocejas900papu <127849583+alejocejas900papu@users.noreply.github.com>
2023-04-04 10:46:49 -0300

    constantes 6

---
+++ constantes 6.py
@@ -0,0 +1,16 @@
+import math
+from re import T
+print ("-------------------------------")
+print ("Ejercicio Condicioneal nuemero 6 ")
+print ("-------------------------------")
+
+m=float
+k=float 
+
+mi=1.609344
+m = float (" ingrese las millas:")
+
+k= m*mi 
+
+print (m , "millas tiene  " , k ,"kilometros")
+
