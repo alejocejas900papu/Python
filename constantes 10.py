f086ae542ccca14a7ea11229a8727ae1210784fd
alejocejas900papu <127849583+alejocejas900papu@users.noreply.github.com>
2023-04-04 10:50:09 -0300

    constantes 10

---
+++ constantes 10.py
@@ -0,0 +1,20 @@
+import math
+from re import T
+print ("-------------------------------")
+print ("Ejercicio Condicioneal nuemero 10 ")
+print ("-------------------------------")
+
+dis , x1, y1 ,z1 ,x1 ,x2 ,x3 =float (input ())
+
+print ("ingresar los  valores de los puntos (x1,y1,z1)")
+x1 = float (input())
+y1 = float (input ())
+z1= float ( input ())
+print ("ingresar los  valores de los puntos (x2,y2,z2)")
+x2 = float (input())
+y2 = float (input ())
+z2= float ( input ())
+
+dis = ((z2-z1)**2 + (y2-y1)**+ (x2-x1)**2)**0.5 
+
+print ( " la distancia total es " , dis )
