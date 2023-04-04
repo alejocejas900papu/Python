ccb591ea0f62bf6877d88684cb04bf58c8766c03
alejocejas900papu <127849583+alejocejas900papu@users.noreply.github.com>
2023-04-04 10:48:42 -0300

    constantes 8

---
+++ constantes 8.py
@@ -0,0 +1,23 @@
+import math
+from re import T
+print ("-------------------------------")
+print ("Ejercicio Condicioneal nuemero 8 ")
+print ("-------------------------------")
+
+d , va , vb ,te = float
+
+print (" ingrese las velocidades del los vuerpos a y b")
+print("a:")
+va= float (input())
+print("b:")
+vb= float (input ())
+
+d=float (print (" ingrese la distancia que los separa ") )
+te=d
+d/(va+vb)
+
+print (" los cuerpos se encontraban , en", te , "segundos" )
+
+
+
+
