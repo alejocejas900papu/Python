8419d5e841783a1ca8b613c61d98e80abe2fd465
alejocejas900papu <127849583+alejocejas900papu@users.noreply.github.com>
2023-04-04 10:25:08 -0300

    costantes 5

---
+++ constantes 5.py
@@ -0,0 +1,13 @@
+import math
+from re import T
+print ("-------------------------------")
+print ("Ejercicio Condicioneal nuemero 5 ")
+print ("-------------------------------")
+
+c=float (print("ingrese costo unitaerio del articulo :"))
+
+d=int (print("ingrese el numero de docenas "))
+
+p=d*12*c 
+
+print ("el precio del articulo es : " ,p)
