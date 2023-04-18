780847019a80d17d72b973165063edd1617a3326
alejocejas900papu <127849583+alejocejas900papu@users.noreply.github.com>
2023-04-18 10:54:54 -0300

    ejercicio12

---
+++ ejercicio12.py
@@ -0,0 +1,16 @@
+import math
+from re import T
+print ("-------------------------------")
+print ("Ejercicio 12 ")
+print ("-------------------------------")
+
+int aumento 
+sueldo=float(input("ingrese el sueldo del empleado"))
+
+if (sueldo <1000 ){
+
+aumento=sueldo*0.15
+sueldo=sueldo+aumento
+}
+
+print ("el sueldo de el empleado es " , sueldo)
