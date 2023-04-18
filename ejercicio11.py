cdb3a317444efa9932a90c8d06959416dcbfcdb1
alejocejas900papu <127849583+alejocejas900papu@users.noreply.github.com>
2023-04-18 10:54:10 -0300

    ejercicio11

---
+++ ejercicio11.py
@@ -0,0 +1,14 @@
+import math
+from re import T
+print ("-------------------------------")
+print ("EjerciCIO 11 ")
+print ("-------------------------------")
+
+cal=float(input("ingrese calificacion :"))
+
+if (cal<=6 )
+
+    print("el alumno esta desaprobado")
+
+else
+    print("el alumno esta aprobado")
