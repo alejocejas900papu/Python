21e34379161ac60eeca311dcf0a3bb8adb6c5690
alejocejas900papu <127849583+alejocejas900papu@users.noreply.github.com>
2023-04-18 10:55:32 -0300

    ejercicio13

---
+++ ejercicio13.py
@@ -0,0 +1,15 @@
+import math
+from re import T
+print ("-------------------------------")
+print ("Ejercicio Condicioneal nuemero 13 ")
+print ("-------------------------------")
+
+año = int(input("ingresa el año"))
+
+
+print("-------------------------------------------------------")
+if (año % 400 == 0) or (año % 4 == 0) and (año % 100 != 0){
+print("El año es bisiesto")
+else:
+print("El año no es bisiesto")
+}
